ebfbd28f66bdf7ebfc9582c35a7b9860f1dadd84
Jacob Lam <jlam18@binghamton.edu>
2019-10-12 00:57:22 -0400

    fixing routes to log exercises

---
+++ app/controllers/api/v1/exercise_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ExerciseController < ApplicationController
+
+  def create
+    byebug
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
        resources :users, only: [:create]
        post '/login', to: 'auth#create'
        get '/profile', to: 'users#profile'
+       post '/log', to: 'exercise#create'
      end
    end
 end
